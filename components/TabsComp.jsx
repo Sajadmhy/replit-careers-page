b08946f646ca894912e6d3028e7a4c2462189a8f
sajadmhy <sajad.mahyaei@gmail.com>
2022-07-08 11:30:41 +0430

    Add files and command tabs

---
+++ components/TabsComp.jsx
@@ -0,0 +1,68 @@
+import styles from '../styles/Tabs.module.css';
+
+
+export default function Tabs() {
+    return (
+        <div className={styles.tabs}>
+                <button className={styles.runButton}>Run <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M3 22v-20l18 10-18 10z"/></svg> </button>
+                <div className={styles.files}>
+                    <div className={styles.head}>
+                        script.js
+                    </div>
+                    <div className={styles.body}>
+                    <li>index.html</li>
+                    <li>styles.css</li>
+                    </div>
+                </div>
+                <div className={styles.editor}>
+                  <code>
+                    <span className={styles.codeBlue}>let</span> {`REPLIT_INTERESTS = [“dev tools”, “creative tools”, `}
+                    {`“programming education”]`}
+                    <br/>
+                    <br/>
+                    <span className={styles.codePink}>function </span><span className={styles.codeYellow}>confirmApplication</span>(
+                    <span className={styles.codeGreen}>name</span>) {`{`}
+                    <br />
+                    <span className={styles.comment}>{`// TODO: implement send to replit`}</span>
+                    <br />
+                    {`}`}
+                    <br/>
+                    <br />
+                    <span className={styles.codePink}>function </span> <span className={styles.codeYellow}>applyToReplit</span> (
+                    <span className={styles.codeGreen}>{`{ name, portfolio, interests }`}</span>
+                    {`) {`}
+                    <br />
+                    <span className={`${styles.codeBlue} ${styles.codeIndent}`}>if</span>
+                    <span>{`(portfolio && interests.some((`}</span><span className={styles.codeGreen}>interest</span> {`)`} {`=>`}
+                    <br />
+                    REPLIT_INTERESTS.includes(interest)
+                    {`)) {`}
+                    <br />
+                    <span className={`${styles.codeBlue} ${styles.codeIndent}`}>{`confirmApplication(name)`}</span>{`}`}
+                    <br />
+                    {`}`}
+                    <br/>
+                    <br/>
+                    <span className={styles.comment}>
+                    {`// usage`}
+                    <br />
+                    {`// applyToReplit({ name: 'you',`}
+                    <br />
+                    {`// portfolio: 'yourportfolio.com',`}
+                    <br />
+                    {`// interests: ['dev tools', 'creative tools', 'engineering']`}
+                    <br />
+                    {`// })`}
+                    <br />
+                    <br />
+                    </span>
+                  </code>
+                </div>
+                <div className={styles.commandLine}>
+                   <div>
+                   ~/applyToReplit$
+                    </div>     
+                </div>
+                </div>
+    )
+}
--- components/TitleComp.jsx
+++ components/TitleComp.jsx
@@ -1,4 +1,5 @@
 import styles from '../styles/Title.module.css';
+import Tabs from './TabsComp';
 
 export default function TitleComp() {
     return (
@@ -12,53 +13,7 @@ export default function TitleComp() {
             </div>
             <button>See open positions</button>
             </div>
-            <div className={styles.tabs}>
-                <button className={styles.runButton}>Run <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M3 22v-20l18 10-18 10z"/></svg> </button>
-                <div className={styles.codeTab}>
-                  <code>
-                    <span className={styles.codeBlue}>let</span> {`REPLIT_INTERESTS = [“dev tools”, “creative tools”, `}
-                    {`“programming education”]`}
-                    <br/>
-                    <br/>
-                    <span className={styles.codePink}>function </span><span className={styles.codeYellow}>confirmApplication</span>(
-                    <span className={styles.codeGreen}>name</span>) {`{`}
-                    <br />
-                    <span className={styles.comment}>{`// TODO: implement send to replit`}</span>
-                    <br />
-                    {`}`}
-                    <br/>
-                    <br />
-                    <span className={styles.codePink}>function </span> <span className={styles.codeYellow}>applyToReplit</span> (
-                    <span className={styles.codeGreen}>{`{ name, portfolio, interests }`}</span>
-                    {`) {`}
-                    <br />
-                    <span className={`${styles.codeBlue} ${styles.codeIndent}`}>if</span>
-                    <span>{`(portfolio && interests.some((`}</span><span className={styles.codeGreen}>interest</span> {`)`} {`=>`}
-                    <br />
-                    REPLIT_INTERESTS.includes(interest)
-                    {`)) {`}
-                    <br />
-                    <span className={`${styles.codeBlue} ${styles.codeIndent}`}>{`confirmApplication(name)`}</span>{`}`}
-                    <br />
-                    {`}`}
-                    <br/>
-                    <br/>
-                    <span className={styles.comment}>
-                    {`// usage`}
-                    <br />
-                    {`// applyToReplit({ name: 'you',`}
-                    <br />
-                    {`// portfolio: 'yourportfolio.com',`}
-                    <br />
-                    {`// interests: ['dev tools', 'creative tools', 'engineering']`}
-                    <br />
-                    {`// })`}
-                    <br />
-                    <br />
-                    </span>
-                  </code>
-                </div>
-            </div>
+            <Tabs/>
         </div>
     )
 }
