5dcc07f638fa0467058b33cbb4f972437a8bdc15
sajadmhy <sajad.mahyaei@gmail.com>
2022-07-10 23:44:49 +0430

    Use Link instead of <a> tag

--- components/TitleComp.jsx
+++ components/TitleComp.jsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Title.module.css';
 import Tabs from './TabsComp';
+import Link from 'next/link';
 
 export default function TitleComp() {
     return (
@@ -11,7 +12,7 @@ export default function TitleComp() {
             <div className={styles.body}>
             Sajad&apos;s on a mission to help Replit bring the next billion software creators online. Building the future of is a team effort.
             </div>
-            <a href="/#cool"><button >See something cool</button></a>
+            <Link href="/#cool"><button >See something cool</button></Link>
             </div>
             <Tabs/>
         </div>
