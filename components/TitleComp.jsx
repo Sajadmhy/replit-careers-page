f562b4a0596c42c52e2869ca50be71ce6d2e07e6
sajadmhy <sajad.mahyaei@gmail.com>
2022-07-11 00:09:28 +0430

    Fix a type in title and add scaling effecting to cursors

--- components/TitleComp.jsx
+++ components/TitleComp.jsx
@@ -10,7 +10,7 @@ export default function TitleComp() {
             <div className={styles.title}>
             <h1>Replit with Sajad</h1>
             <div className={styles.body}>
-            Sajad&apos;s on a mission to help Replit bring the next billion software creators online. Building the future of is a team effort.
+            Sajad&apos;s on a mission to help Replit bring the next billion software creators online. Building the future is a team effort.
             </div>
             <Link href="/#cool"><button >See something cool</button></Link>
             </div>
